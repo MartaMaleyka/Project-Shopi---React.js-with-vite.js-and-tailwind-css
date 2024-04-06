33b7b5c6fefa944d27369e6ad204cfe9f16ffaa4
MartaMaleyka <martamagallon1994@gmail.com>
2024-04-05 19:46:29 -0500

    Consumiendo API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,15 +1,15 @@
-const Card = () => {
+const Card = (data) => {
    return(
     <div className='flex'>
-        <div className='bg-gray-50 cursor-pointer m-5 w-56 h-60 rounded-sm active:bg-gray-50/60 focus:outline-none focus:ring-white focus:ring-2'>
+        <div className='bg-gray-50 cursor-pointer m-5 w-60 h-72 rounded-sm active:bg-gray-50/60 focus:outline-none focus:ring-white focus:ring-2'>
             <figure className='relative mb-3 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-gray-50/60 rounded-lg m-2 px-3 py-0.5'>Fashion</span>
-                <img className=' w-full h-full object-cover rounded-lg' src="https://images.pexels.com/photos/15673005/pexels-photo-15673005/free-photo-of-sesion-xv-anos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <span className='absolute bottom-0 left-0 bg-gray-50/60 rounded-lg m-2 px-3 py-0.5'>{data.data.category.name}</span>
+                <img className=' w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
                 <div className='absolute m-2 top-0 right-0 flex justify-center items-center bg-cyan-600 text-white w-6 h-6 rounded-full p-1 hover:bg-cyan-400'>+</div>
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm m-2 font-light'>Vestido de Encaje</span>
-                <span className='text-sm m-2 font-bold'>$300</span>
+                <span className='text-sm m-2 font-light'>{data.data.title}</span>
+                <span className='text-sm m-2 font-bold'>${data.data.price}</span>
             </p>
         </div>
     </div>
