e4ce1344643f3775ca03686eda3aae5c412aa2ee
MartaMaleyka <martamagallon1994@gmail.com>
2024-04-20 19:03:24 -0500

    Pagina de mis ordenes

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,18 +1,23 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 import OrderCard from '../../Components/OrderCard'
 
 function MyOrder() {
+  const context = useContext(ShoppingCartContext)
 
-  const context=useContext(ShoppingCartContext)
-
-    return (
-      <>
-        <Layout>
-          My Order
-          <div className='flex flex-col w-80'>
-          {
+  return (
+    <Layout>
+      <div className='flex items-center justify-center relative w-80 mb-6'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer' />
+        </Link>
+        <h1>My Order</h1>
+      </div>
+      <div className='flex flex-col w-80'>
+        {
           context.order?.slice(-1)[0].products.map(product => (
             <OrderCard
               key={product.id}
@@ -22,12 +27,10 @@ function MyOrder() {
               price={product.price}
             />
           ))
-          }
-          </div>
-        </Layout>
-      </>
-    )
-  }
-  
-  export default MyOrder
-  
+        }
+      </div>
+    </Layout>
+  )
+}
+
+export default MyOrder
